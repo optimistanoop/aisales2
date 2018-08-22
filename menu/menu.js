824dcda072226cd7d4a0ce8e132661cf7cfcbd62
Mahesh Kumar <mahesh26.kumar@ril.com>
2018-08-23 00:31:55 +0530

    avtar showing as per mood

--- menu/menu.js
+++ menu/menu.js
@@ -2,7 +2,15 @@ aisales.controller('menuCtrl',function ($scope,$rootScope, $location) {
   $rootScope.cartData ={ items:[], total:0};
   if($rootScope.menudata && $rootScope.signIn != 'Sign In'){
     $rootScope.recommendedFood =  $rootScope.menudata.recommendedFood; 
-    $rootScope.menuItems = $rootScope.menudata.menuItems; 
+    $rootScope.menuItems = $rootScope.menudata.menuItems;
+    $scope.gender = $rootScope.menudata.faceData.faces[0].gender;
+    var emotion = $rootScope.menudata.faceData.faces[0].emotions[0];
+    if (emotion == "happy" || emotion == "angry" || emotion == "neutral" || emotion == "sad") {
+      $scope.mood = emotion;
+    }
+    else {
+      $scope.mood = 'neutral';
+    }
   }else{
     //$location.url('/payment');
     //$rootScope.uploadStream()
